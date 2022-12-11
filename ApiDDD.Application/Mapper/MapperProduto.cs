1a59d7ea9c64bb1fb0f8322a88e1ab0aabc9e350
arthurfs010 <arthurfs96@gmail.com>
2022-12-11 16:18:48 -0300

    Ajuste Map

--- ApiDDD.Application/Mapper/MapperProduto.cs
+++ ApiDDD.Application/Mapper/MapperProduto.cs
@@ -14,14 +14,14 @@ namespace ApiDDD.Application.Mapper
 
 		private void ProdutoMap()
 		{
-			CreateMap<ProdutoDTO, Produto>()
-				.ForMember(dest => dest.Codigo, opt => opt.MapFrom(pdto => pdto.Codigo))
+			CreateMap<ProdutoDTO, Produto>();
+				/*.ForMember(dest => dest.Codigo, opt => opt.MapFrom(pdto => pdto.Codigo))
 				.ForMember(dest => dest.Descricao, opt => opt.MapFrom(pdto => pdto.Descricao))
 				.ForMember(dest => dest.DataValidade, opt => opt.MapFrom(pdto => pdto.DataValidade))
 				.ForMember(dest => dest.DataFabricacao, opt => opt.MapFrom(pdto => pdto.DataFabricacao))
 				.ForMember(dest => dest.CodigoFornecedor, opt => opt.MapFrom(pdto => pdto.CodigoFornecedor))
 				.ForMember(dest => dest.DescricaoFornecedor, opt => opt.MapFrom(pdto => pdto.DescricaoFornecedor))
-				.ForMember(dest => dest.CNPJFornecedor, opt => opt.MapFrom(pdto => pdto.CNPJFornecedor)); 
+				.ForMember(dest => dest.CNPJFornecedor, opt => opt.MapFrom(pdto => pdto.CNPJFornecedor)); */
         }
 	}
 }
